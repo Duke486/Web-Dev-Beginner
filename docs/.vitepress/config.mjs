66d659aa151f16e63da52cf4056b83a0320926c2
Duke486 <duke_486@outlook.com>
2025-03-18 20:42:49 +0800

    改善readme中图片样式，增加about页面

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -8,7 +8,7 @@ export default defineConfig({
     description: "Quick start to front & back-end Dev",
     logo: '/assets/logo.png',
     footer: {
-        message: '基于 MIT 许可发布', copyright: `版权所有 © 2019-${new Date().getFullYear()} Duke486`
+        message: '基于 MIT 许可发布', copyright: `版权所有 © 2024-${new Date().getFullYear()} Duke486`
     },
 
 
@@ -38,9 +38,9 @@ export default defineConfig({
 
 
         nav: [
-            {text: 'Home', link: '/'},
-            {text: 'Schedule', link: '/schedule'},
-            {text: 'Github', link: 'https://github.com/Duke486/Web-Dev-Beginner'},
+            {text: '主页', link: '/'},
+            {text: '时间表', link: '/schedule'},
+            {text: 'Github仓库', link: 'https://github.com/Duke486/Web-Dev-Beginner'},
         ],
 
         sidebar: [
@@ -63,72 +63,72 @@ export default defineConfig({
                     {text: 'Homework', link: '/week-1/homework'},
                 ]
             },
-            {
-                text: 'Week-2',
-                items: [
-                    {text: 'Doc', link: '/week-2/doc'},
-                    {text: 'Lab', link: '/week-2/lab'},
-                    {text: 'Homework', link: '/week-2/homework'},
-                ]
-            },
-            {
-                text: 'Week-3',
-                items: [
-                    {text: '前端Doc', link: '/week-3/前端doc'},
-                    {text: '前端Lab', link: '/week-3/前端lab'},
-                    {text: '前端Homework', link: '/week-3/前端homework'},
-                    {text: '后端Doc', link: '/week-3/后端doc'},
-                    {text: '后端Lab', link: '/week-3/后端lab'},
-                    {text: '后端Homework', link: '/week-3/后端homework'},
-                ]
-            },
-                        {
-                text: 'Week-4',
-                items: [
-                    {text: '前端Doc', link: '/week-4/前端doc'},
-                    {text: '前端Lab', link: '/week-4/前端lab'},
-                    {text: '前端Homework', link: '/week-4/前端homework'},
-                    {text: '后端Doc', link: '/week-4/后端doc'},
-                    {text: '后端Lab', link: '/week-4/后端lab'},
-                    {text: '后端Homework', link: '/week-4/后端homework'},
-                ]
-            },
-            {
-                text: 'Week-5',
-                items: [
-                    {text: '前端Doc', link: '/week-5/前端doc'},
-                    {text: '前端Lab', link: '/week-5/前端lab'},
-                    {text: '前端Homework', link: '/week-5/前端homework'},
-                    {text: '后端Doc', link: '/week-5/后端doc'},
-                    {text: '后端Lab', link: '/week-5/后端lab'},
-                    {text: '后端Homework', link: '/week-5/后端homework'},
-                ]
-            },
-            {
-                text: 'Week-6',
-                items: [
-                    {text: '前端Doc', link: '/week-6/前端doc'},
-                    {text: '前端Lab', link: '/week-6/前端lab'},
-                    {text: '前端Homework', link: '/week-6/前端homework'},
-                    {text: '后端Doc', link: '/week-6/后端doc'},
-                    {text: '后端Lab', link: '/week-6/后端lab'},
-                    {text: '后端Homework', link: '/week-6/后端homework'},
-                ]
-            },
-            {
-                text: 'Week-7',
-                items: [
-                    {text: 'Doc', link: '/week-7/doc'},
-                    {text: 'Lab', link: '/week-7/lab'},
-                ]
-            },
-            {
-                text: 'Week-8',
-                items: [
-                    {text: '前端', link: '/week-8/前端.md'},
-                    {text: '后端', link: '/week-8/后端.md'},
-                ]
-            },
+            // {
+            //     text: 'Week-2',
+            //     items: [
+            //         {text: 'Doc', link: '/week-2/doc'},
+            //         {text: 'Lab', link: '/week-2/lab'},
+            //         {text: 'Homework', link: '/week-2/homework'},
+            //     ]
+            // },
+            // {
+            //     text: 'Week-3',
+            //     items: [
+            //         {text: '前端Doc', link: '/week-3/前端doc'},
+            //         {text: '前端Lab', link: '/week-3/前端lab'},
+            //         {text: '前端Homework', link: '/week-3/前端homework'},
+            //         {text: '后端Doc', link: '/week-3/后端doc'},
+            //         {text: '后端Lab', link: '/week-3/后端lab'},
+            //         {text: '后端Homework', link: '/week-3/后端homework'},
+            //     ]
+            // },
+            //             {
+            //     text: 'Week-4',
+            //     items: [
+            //         {text: '前端Doc', link: '/week-4/前端doc'},
+            //         {text: '前端Lab', link: '/week-4/前端lab'},
+            //         {text: '前端Homework', link: '/week-4/前端homework'},
+            //         {text: '后端Doc', link: '/week-4/后端doc'},
+            //         {text: '后端Lab', link: '/week-4/后端lab'},
+            //         {text: '后端Homework', link: '/week-4/后端homework'},
+            //     ]
+            // },
+            // {
+            //     text: 'Week-5',
+            //     items: [
+            //         {text: '前端Doc', link: '/week-5/前端doc'},
+            //         {text: '前端Lab', link: '/week-5/前端lab'},
+            //         {text: '前端Homework', link: '/week-5/前端homework'},
+            //         {text: '后端Doc', link: '/week-5/后端doc'},
+            //         {text: '后端Lab', link: '/week-5/后端lab'},
+            //         {text: '后端Homework', link: '/week-5/后端homework'},
+            //     ]
+            // },
+            // {
+            //     text: 'Week-6',
+            //     items: [
+            //         {text: '前端Doc', link: '/week-6/前端doc'},
+            //         {text: '前端Lab', link: '/week-6/前端lab'},
+            //         {text: '前端Homework', link: '/week-6/前端homework'},
+            //         {text: '后端Doc', link: '/week-6/后端doc'},
+            //         {text: '后端Lab', link: '/week-6/后端lab'},
+            //         {text: '后端Homework', link: '/week-6/后端homework'},
+            //     ]
+            // },
+            // {
+            //     text: 'Week-7',
+            //     items: [
+            //         {text: 'Doc', link: '/week-7/doc'},
+            //         {text: 'Lab', link: '/week-7/lab'},
+            //     ]
+            // },
+            // {
+            //     text: 'Week-8',
+            //     items: [
+            //         {text: '前端', link: '/week-8/前端.md'},
+            //         {text: '后端', link: '/week-8/后端.md'},
+            //     ]
+            // },
 
 
         ],
--- docs/about.md
+++ docs/about.md
@@ -1,5 +1,10 @@
 ---
-lastUpdated: 2024-10-13
+lastUpdated: 2025-3-18
 editLink: true
 ---
-# 这是关于页面
+
+# 出发，前进
+
+欢迎，这里是 Duke486 的个人项目在线阅读网站。您可以在[博客主页](https://duke486.com/)上面找到[我](https://duke486.com/about/)!
+
+本项目在[Github](https://github.com/Duke486/Web-Dev-Beginner)上开源，同时修订后在[杭电wiki](https://hdu-cs.wiki/)发布，喜欢的话请点亮Star哦。
